b2e46354558a41b4d274fb01937496b04502c3c7
nathan hsiao <nathanhsiao123@gmail.com>
2024-04-20 13:46:34 -0400

    Update hi.sql

--- hi.sql
+++ hi.sql
@@ -12,3 +12,17 @@ ADD column yearArchived INTEGER;
 
 ALTER TABLE data_upload_system_archivehistory
 ADD column month INTEGER;
+
+CREATE TABLE IF NOT EXISTS "projections_update_system_projectionsarchive" (
+	"id"	INTEGER NOT NULL,
+	"projection"	NUMERIC,
+	"accE_id"	TEXT,
+	"org_id"	INTEGER,
+	"comments"	TEXT,
+	"account"	TEXT,
+	"lastUpdatedFiscalMonth"	INTEGER,
+	"lastUpdatedYear"	INTEGER,
+	"activity"	TEXT,
+	"location"	TEXT,
+	"program"	TEXT
+, projectionMonth integer, projectionYear integer);
